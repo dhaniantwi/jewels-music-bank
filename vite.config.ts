4adf64e01216f2920d90622d9a8b71100fd94078
dhaniantwi <antwidaniel897@gmail.com>
2026-08-30 10:43:15 -0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/jewels-music-bank/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
