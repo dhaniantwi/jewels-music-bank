496eee13c61e602600a1d6e7c8511dcc27c4a79f
dhaniantwi <antwidaniel897@gmail.com>
2026-09-05 14:13:42 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -491,7 +491,14 @@ const [showMDLogin, setShowMDLogin] =
 
   return (
     <div className="min-h-screen flex flex-col justify-between text-[#1d1d1f] font-sans pb-12 sm:pb-16 selection:bg-[#007aff]/20 selection:text-[#007aff]">
-
+{showMDLogin && (
+  <MDLogin
+    onLoginSuccess={() => {
+      setShowMDLogin(false);
+      setActiveRole('admin_md');
+    }}
+  />
+)} 
       <div>
 
         {/* ======================================================
