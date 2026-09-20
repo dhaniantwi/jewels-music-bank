ca99e0b295d4eb3c82ca92a8ce8e1922a4fdf6e9
dhaniantwi <antwidaniel897@gmail.com>
2026-09-20 13:11:10 +0000

    Log message on stage mode button click

--- src/App.tsx
+++ src/App.tsx
@@ -1321,9 +1321,10 @@ const handlePhotoSelected = async (
                   setIsToolsModalOpen(true)
                 }
 
-                openStageMode={() =>
-                  setIsStageModeOpen(true)
-                }
+               openStageMode={() => {
+  console.log('STAGE MODE BUTTON CLICKED');
+  setIsStageModeOpen(true);
+}}
               />
 
             )}
